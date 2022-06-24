ab60f61e6e1c92952890feb596a4f3f256bef8ae
Sanmi01 <supersanmi01@gmail.com>
2022-06-24 22:01:13 +0100

    made changes

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-let firstNum = parseFloat(prompt("Enter the first number:"))
-
-
-while ( (isNaN(firstNum)) ||  (firstNum == "") ||  (firstNum == null) ) {
-    firstNum = parseFloat(prompt("Invalid input. Please enter a valid input (number) for the first number:"))
-}
-
-let secondNum = parseFloat(prompt("Enter the second number:"))
-
-while ( (isNaN(secondNum)) ||  (secondNum == "") ||  (secondNum == null) ) {
-    secondNum = parseFloat(prompt("Invalid input. Please enter a valid input (number) for the second number:"))
-}
-
-let operation = prompt("Enter operator from the list below: +, -, *, /, %")
-let answer = ""
-
-while (operation !== "+" && operation !== "-" && operation !== "*" && operation !== "/" && operation !== "%") {
-    operation = prompt("Invalid input. Please enter operator from the list below: +, -, *, /, %")
-}
-
 function calculation() {
+    let firstNum = parseFloat(prompt("Enter the first number:"))
+    while ( (isNaN(firstNum)) ||  (firstNum == "") ||  (firstNum == null) ) {
+        firstNum = parseFloat(prompt("Invalid input. Please enter a valid input (number) for the first number:"))
+    }
+    
+    let secondNum = parseFloat(prompt("Enter the second number:"))
+    while ( (isNaN(secondNum)) ||  (secondNum == "") ||  (secondNum == null) ) {
+        secondNum = parseFloat(prompt("Invalid input. Please enter a valid input (number) for the second number:"))
+    }
+    
+    let operation = prompt("Enter operator from the list below: +, -, *, /, %")
+    let answer = ""
+    while (operation !== "+" && operation !== "-" && operation !== "*" && operation !== "/" && operation !== "%") {
+        operation = prompt("Invalid input. Please enter operator from the list below: +, -, *, /, %")
+    }
+
     if (operation === "+") {
         answer = firstNum + secondNum
         alert(answer)
@@ -37,4 +33,9 @@ function calculation() {
     } 
 }
 
-calculation()
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+    calculation();
+});
+
+calculation();
