48843123897bbf27d7b94b8a296e9364cb7ce140
Sanmi01 <supersanmi01@gmail.com>
2022-06-24 21:51:26 +0100

    made changes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let firstNum = prompt("Enter the first number:")
+let firstNum = parseFloat(prompt("Enter the first number:"))
 
 
 while ( (isNaN(firstNum)) ||  (firstNum == "") ||  (firstNum == null) ) {
-    firstNum = prompt("Invalid input. Please enter a valid input (number) for the first number:")
+    firstNum = parseFloat(prompt("Invalid input. Please enter a valid input (number) for the first number:"))
 }
 
-let secondNum = prompt("Enter the second number:")
+let secondNum = parseFloat(prompt("Enter the second number:"))
 
 while ( (isNaN(secondNum)) ||  (secondNum == "") ||  (secondNum == null) ) {
-    secondNum = prompt("Invalid input. Please enter a valid input (number) for the second number:")
+    secondNum = parseFloat(prompt("Invalid input. Please enter a valid input (number) for the second number:"))
 }
 
 let operation = prompt("Enter operator from the list below: +, -, *, /, %")
@@ -20,7 +20,7 @@ while (operation !== "+" && operation !== "-" && operation !== "*" && operation 
 
 function calculation() {
     if (operation === "+") {
-        answer = parseFloat(firstNum) + parseFloat(secondNum)
+        answer = firstNum + secondNum
         alert(answer)
     } else if (operation === "-") {
         answer = firstNum - secondNum
